ca852c3ec60d93657db40df2fdffe9d71688f00c
kaviyarasan1993 <kaviyarasanit@gmail.com>
2020-12-21 12:33:36 +0530

    RAIN-1893 bug fix 2 for zero bill

--- billing-service/src/main/java/org/egov/demand/service/BillServicev2.java
+++ billing-service/src/main/java/org/egov/demand/service/BillServicev2.java
@@ -341,7 +341,7 @@ public class BillServicev2 {
 			BigDecimal totalCollection = demand.getDemandDetails().stream().map(DemandDetail::getCollectionAmount)
 					.reduce(BigDecimal.ZERO, BigDecimal::add);
 			if (totoalTax.compareTo(totalCollection) > 0 || totoalTax.compareTo(totalCollection) == 0
-					&& demand.getAuditDetails().getCreatedTime() == demand.getAuditDetails().getLastModifiedTime())
+					&& demand.getAuditDetails().getCreatedTime().equals(demand.getAuditDetails().getLastModifiedTime()))
 				return true;
 			else
 				return false;
